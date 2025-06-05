68ffe676b9a4ffc58d08473dc783f0abd0bfd3b3
pipin <prustolivgra@gmail.com>
2025-06-05 15:40:38 +0200

    # Turns this folder into a Python module. # Enables imports like `from backend.app.models import User` # and keeps the structure modular and clean.

---
+++ backend/app/enums/__init__.py
@@ -0,0 +1,5 @@
+from .user_role import UserRole
+
+__all__ = [
+    "UserRole",
+]
---
+++ backend/app/models/__init__.py
@@ -0,0 +1,13 @@
+from .user import User
+from .client import Client
+from .worker import Worker
+from .broker import Broker
+from .admin import Admin
+
+__all__ = [
+    "User",
+    "Client",
+    "Worker",
+    "Broker",
+    "Admin",
+]
