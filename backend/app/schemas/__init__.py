f63154b230aff5705d4b681108f42c6e2d56fec0
pipin <prustolivgra@gmail.com>
2025-06-09 17:37:17 +0200

    - Imported RefreshToken from schemas.sessions into root schemas namespace - Allows direct access like: from schemas import RefreshToken

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,5 +1,9 @@
 from backend.app.schemas.base import SchemaBase
+from backend.app.schemas.sessions import RefreshToken
+
+RefreshTokenOut = RefreshToken.out
 
 __all__ = [
     "SchemaBase",
+    "RefreshToken", "RefreshTokenOut"
 ]
