9cac725d24d17966aebfc428c9468798aefe129c
pipin <prustolivgra@gmail.com>
2025-06-13 14:48:04 +0200

    chore(services): replace wildcard imports with explicit imports and update __all__ list for all entity services

--- backend/app/services/entities/__init__.py
+++ backend/app/services/entities/__init__.py
@@ -1,12 +1,28 @@
-from .user import *
-from .admin import *
-from .broker import *
-from .client import *
-from .worker import *
+from .user import UserService, UserUtilService, UserFilterService, UserInterfaceService
+from .admin import AdminService, AdminInterfaceService, AdminFilterService, AdminUtils
+from .broker import BrokerUtils, BrokerService, BrokerInterfaceService, BrokerFilterService
+from .client import ClientServices, ClientUtilService, ClientInterfaceService, ClientFilterService
+from .worker import WorkerService, WorkerInterfaceService, WorkerUtilsService, WorkerFilterService
 
 __all__ = [
     "UserService",
     "UserUtilService",
     "UserInterfaceService",
-    "UserFilterService"
+    "UserFilterService",
+    "AdminService",
+    "AdminInterfaceService",
+    "AdminFilterService",
+    "AdminUtils",
+    "BrokerUtils",
+    "BrokerService",
+    "BrokerInterfaceService",
+    "BrokerFilterService",
+    "WorkerService",
+    "WorkerInterfaceService",
+    "WorkerUtilsService",
+    "WorkerFilterService",
+    "ClientUtilService",
+    "ClientInterfaceService",
+    "ClientFilterService",
+    "ClientServices"
 ]
