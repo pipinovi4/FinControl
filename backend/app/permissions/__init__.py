2848af1993ee601f324fe878dc079f858997ed43
pipin <prustolivgra@gmail.com>
2025-06-16 18:28:11 +0200

    refactor(permissions): temporarily remove require_role from public API

--- backend/app/permissions/__init__.py
+++ backend/app/permissions/__init__.py
@@ -1,10 +1,8 @@
-from .checker import require_role
 from .enums import PermissionRole
 # TODO Import middlewares and models when it will implemented
 # from .middleware import
 # from .models import
 
 __all__ = [
-    "require_role",
     "PermissionRole",
 ]
