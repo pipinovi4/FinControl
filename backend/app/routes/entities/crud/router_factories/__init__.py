af12ffa9c03f8255399542d61fa4a25601a4ed34
pipin <prustolivgra@gmail.com>
2025-06-20 16:58:25 +0200

    chore(crud): expose router factories via __init__.py for cleaner imports

---
+++ backend/app/routes/entities/crud/router_factories/__init__.py
@@ -0,0 +1,11 @@
+from .create_router_factory import create_router_factory
+from .read_router_factory import read_router_factory
+from .update_router_factory import update_router_factory
+from .delete_router_factory import delete_router_factory
+
+__all__ = [
+    "create_router_factory",
+    "read_router_factory",
+    "update_router_factory",
+    "delete_router_factory"
+]
