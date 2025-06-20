cac63e457a649fa59f8d58b82120ec2ac65b92e7
pipin <prustolivgra@gmail.com>
2025-06-20 18:57:28 +0200

    feat(init.py): Was added re-export ResetPasswordService in directory backend/app/services/auth/

--- backend/app/services/auth/__init__.py
+++ backend/app/services/auth/__init__.py
@@ -5,11 +5,13 @@ from .refresh_token import RefreshTokenService
 from .access_token import AccessTokenService
 from .utils import generate_token_pair
 from .token_manager import TokenManager
+from .reset_password import ResetPasswordService
 
 __all__ = [
     "PasswordService",
     "RefreshTokenService",
     "AccessTokenService",
     "generate_token_pair",
-    "TokenManager"
+    "TokenManager",
+    "ResetPasswordService"
 ]
