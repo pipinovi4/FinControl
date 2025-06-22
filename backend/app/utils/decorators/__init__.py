2e08a12322ee5243f7214a9ccbde18c11d6af7e6
pipin <prustolivgra@gmail.com>
2025-06-22 22:18:23 +0200

    refactor(decorators): Added websocket handle exceptions to re-export in __init__.py

--- backend/app/utils/decorators/__init__.py
+++ backend/app/utils/decorators/__init__.py
@@ -1,7 +1,9 @@
 from .db import handle_exceptions
 from .route import handle_route_exceptions
+from .websocket import handle_ws_exceptions
 
 __all__ = [
     "handle_exceptions",
-    "handle_route_exceptions"
+    "handle_route_exceptions",
+    "handle_ws_exceptions"
 ]
