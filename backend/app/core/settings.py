23f8c44ee720579104f10eceba76f6cf81872f16
pipin <prustolivgra@gmail.com>
2025-06-22 03:07:23 +0200

    chore(config): update DATABASE_URL to use asyncpg for async database connections
    - Changed the `DATABASE_URL` property to use `postgresql+asyncpg` instead of `postgresql+psycopg2` for asynchronous database connections.
    - This change aligns with the async database setup to utilize `asyncpg` for improved performance in async operations.

--- backend/app/core/settings.py
+++ backend/app/core/settings.py
@@ -30,7 +30,7 @@ class Settings(BaseSettings):
     @property
     def DATABASE_URL(self) -> str:
         return (
-            f"postgresql+psycopg2://{self.DB_USER}:{self.DB_PASSWORD}"
+            f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}"
             f"@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
         )
 
