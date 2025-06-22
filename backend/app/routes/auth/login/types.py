f6fb3386a22c1ac9d2ed4560efb6b2c94c6f2646
pipin <prustolivgra@gmail.com>
2025-06-22 18:39:59 +0200

    refactor: Removed LoginRequestT because that isn't necessary cause any type of login require request

--- backend/app/routes/auth/login/types.py
+++ backend/app/routes/auth/login/types.py
@@ -1,16 +1,9 @@
 from enum import Enum
 
-from backend.app.schemas.auth import LoginRequest
-
-
 class LoginTypes(Enum):
     BOT = "BOT"
     WEB = "WEB"
 
-LoginRequestT = LoginRequest | None
-
-
 __all__ = [
-    "LoginRequestT",
     "LoginTypes",
 ]
