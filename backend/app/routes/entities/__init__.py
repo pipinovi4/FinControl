89d8c73c2232e1cc7ccb4d44225bc32b4681ac08
pipin <prustolivgra@gmail.com>
2025-06-22 21:56:30 +0200

    feat(entities): Added re-export create_analyze_websocket_router

--- backend/app/routes/entities/__init__.py
+++ backend/app/routes/entities/__init__.py
@@ -1,7 +1,8 @@
 from .crud import create_crud_router
-from .analyze import create_analyze_router
+from .analyze import create_analyze_router, create_analyze_websocket_router
 
 __all__ = [
     "create_crud_router",
-    "create_analyze_router"
+    "create_analyze_router",
+    "create_analyze_websocket_router"
 ]
