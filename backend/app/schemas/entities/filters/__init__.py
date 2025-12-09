c6399921a52ee26d7d29df882bbe91399f49568e
pipinovi4 <prustolivgra@gmail.com>
2025-12-09 15:16:51 +0100

    refactor(worker_dashboard): Good Fucking Morning 3.0

--- backend/app/schemas/entities/filters/__init__.py
+++ backend/app/schemas/entities/filters/__init__.py
@@ -1,13 +1,13 @@
 from .user_filter_schema import UserFilterSchema
 from .admin_filter_schema import AdminFilterSchema
 from .broker_filter_schema import BrokerFilterSchema
-from .application_filter_schema import ClientFilterSchema
+from .application_filter_schema import ApplicationFilterSchema
 from .worker_filter_schema import WorkerFilterSchema
 
 __all__ = [
     "UserFilterSchema",
     "AdminFilterSchema",
     "BrokerFilterSchema",
-    "ClientFilterSchema",
+    "ApplicationFilterSchema",
     "WorkerFilterSchema",
 ]
