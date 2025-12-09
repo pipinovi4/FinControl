2916946a0c5c194d90f1b800b8ce6a474facaa0b
pipinovi4 <prustolivgra@gmail.com>
2025-12-09 10:09:34 +0100

    refactor(application): remove old callbacks module

--- bot/handlers/application/__init__.py
+++ bot/handlers/application/__init__.py
@@ -1,15 +1,13 @@
 from .finish import _finish_wizard
-from .router import handle_application_message
 from .prompt import get_prompt, send_step_prompt, wipe_last_prompt
 from .utils import resolve_canonical
-from .callbacks import handle_progress_callback
+from .router_selector import get_router
 
 __all__ = [
     "_finish_wizard",
-    "handle_application_message",
     "send_step_prompt",
     "get_prompt",
     "resolve_canonical",
     "wipe_last_prompt",
-    "handle_progress_callback"
+    "get_router"
 ]
